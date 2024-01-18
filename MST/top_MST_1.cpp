ec2a871455265d504677bc47b67b0595368e9b66
agks <140994215+agks12@users.noreply.github.com>
2024-01-18 22:39:42 +0900

    Update top_MST_1.cpp

--- MST/top_MST_1.cpp
+++ MST/top_MST_1.cpp
@@ -52,3 +52,59 @@ int main() {
 
 	return 0;
 }
+//크루스칼
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+struct Node {
+	int from, to, cost;
+	bool operator<(Node right)const {
+		if (cost > right.cost)return true;
+		if (cost < right.cost)return false;
+		if (to > right.to)return true;
+		if (to < right.to)return false;
+		if (from > right.from)return true;
+		if (from < right.from)return false;
+		return false;
+	}
+};
+
+//MST => 가중치기준으로 sort해서 작은거 부터 연결하면 N-1개 연결되었을때가 정답 자동으로 최소로 연결된거
+// 이게 kruskal 방식 
+priority_queue<Node> pq;
+int parent[10001];
+
+int Find(int num) {
+	if (num == parent[num])return num;
+	return Find(parent[num]);
+}
+//Union 간략화 함
+int kuruskal(int n) {
+	for (int i = 0; i <= n; i++)parent[i] = i;
+	int sum = 0;
+	while (!pq.empty()) {
+		Node now = pq.top(); pq.pop();
+		int rootA = Find(now.from);
+		int rootB = Find(now.to);
+		if (rootA == rootB)continue;
+		sum += now.cost;
+		parent[rootA] = rootB;
+	}
+	return sum;
+}
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		pq.push({ a,b,c });
+	}
+	int ans = kuruskal(n);
+	cout << ans;
+
+	return 0;
+}
