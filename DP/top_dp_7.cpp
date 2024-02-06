4b7cdf7d7a1bab0515029b34ec38e33ee55362df
agks <140994215+agks12@users.noreply.github.com>
2024-02-06 23:13:48 +0900

    Update top_dp_7.cpp

--- DP/top_dp_7.cpp
+++ DP/top_dp_7.cpp
@@ -1,41 +1,42 @@
 //Bottom-Up 
 #include<iostream>
 #include<vector>
-
 using namespace std;
-int N, M, H;
-vector<int>v[51];
-long memo[51][1001];
 
-
+int n, m, h;
+vector<int> v[51];
+long dp[51][1001];
 int main() {
-	//freopen("10.in", "r", stdin);
-	//freopen("10.out", "w", stdout);
-	cin >> N >> M >> H;
-	for (int i = 1; i < N+1; i++) {
-		for (int j = 0; j < M; j++) {
-			int now;
-			cin >> now;
-			if (now != -1) {
-				v[i].push_back(now);
-			}
-		}
-	}
-	for (int i = 0; i <= N; i++)memo[i][0] = 1;
-	for (int i = 1; i < N+1; i++) {
-		for (int j = 1; j < H + 1; j++) {
-			for (int k = 0; k < v[i].size(); k++) {
-				int block = v[i][k];
-				if (j - block >= 0) {
-					memo[i][j] = memo[i][j] + memo[i - 1][j - block];
-				}
-				
-			}
-			memo[i][j] = memo[i][j] + memo[i - 1][j];
+	dp[0][0] = 1;// 초기화
+	cin >> n >> m >> h;
+	for (int i = 1; i <= n; i++) {
+		dp[i][0] = 1;// 초기화
+		for (int j = 0; j < m; j++) {
+			int now; cin >> now;
+			if (now == -1)continue;
+			v[i].push_back(now);
 		}
 	}
 
-	cout << memo[N][H];
+	for (int i = 1; i <= n; i++) {//사람수
+		for (int j = 1; j <= h; j++) { // 높이 수(제한)
+			for (int k = 0; k < v[i].size(); k++) { // 벽돌 개수
+				int blockSize = v[i][k]; // 지금 꺼낸 벽돌 길이
+				if (blockSize > j)continue; // 지금 꺼낸 벽돌 길이가 쌓인 벽돌 보다 킬면 
+				// 지금 내가 벽돌 높이 j만큼 쌓을 경우는 내 전사람이 높이 j-내벽돌길이
+				// 만큼 쌓은 경우 + 1 됨(초기화 할때 [i][0]인덱스만 1했으므로 처음 j-blockSize가 0일때 
+				// +1되고 그 다음부터는 처음 쌓은 벽돌(예 j=3)인덱스 만큼 값이 나올때 추가됨
+				// 즉 예를들어dp[i][j0~h]나열하면 1 0 0 0 2 0 3 0 0 0 0 4 . . . ..가면서 벽돌 높이h되면 최종 경우의수 나옴   
+				// -> 누적되서 다음 번쨰 벽돌높이 할때는 + 2 되고하는듯
+				// 0부터 시작해서 가능한듯 
+				dp[i][j] = dp[i][j] + dp[i - 1][j - blockSize];
+			}
+			dp[i][j] = dp[i][j] + dp[i - 1][j]; // 이건 그냥 dp가져다쓰는건가 top다운에서 dp!=0인경우 반환하는 것처럼
+			// 1번 부터 시작해서 이것도 가능한듯 2번은 1번의 경우 가져다 쓰는듯
+			// 그래서 답이 n번쨰 애가 답인듯
+		}
+	}
+	cout << dp[n][h];
 	return 0;
 }
  
