920ec247c75dc006c8b8f18f2e28fd8ab207c1ea
agks <140994215+agks12@users.noreply.github.com>
2024-02-09 03:07:35 +0900

    Update top_Dijkstra_7.cpp

--- Dijkstra/top_Dijkstra_7.cpp
+++ Dijkstra/top_Dijkstra_7.cpp
@@ -11,11 +11,15 @@ struct node {
 		return false;
 	}
 };
-int visited[200001];
-int dist[200001];
-vector<node> v[200001];
+int visited[100001];
+int dist[100001];
+vector<node> v[100001];
 int c, p, k, a, b;
-void djk(int a, int b) {
+void djk(int a) {
+	for (int i = 0; i <= p; i++) {
+		dist[i] = 1e9;
+		visited[i] = 0;
+	}
 	priority_queue<node> q;
 	q.push({ a,0 });
 	dist[a] = 0;
@@ -23,12 +27,13 @@ void djk(int a, int b) {
 	while (!q.empty()) {
 		node now = q.top(); q.pop();
 		if (visited[now.to] == 1)continue;
+		visited[now.to] = 1;
 		for (int i = 0; i < v[now.to].size(); i++) {
 			node next = v[now.to][i];
 			//if (visited[next.to] == 1)continue;
 			if (dist[now.to] + next.cost > dist[next.to])continue;
 			dist[next.to] = dist[now.to] + next.cost;
-			visited[next.to] = 1;
+			//visited[next.to] = 1;
 			q.push({ next.to,dist[next.to] });
 		}
 	}
@@ -37,14 +42,21 @@ int main()
 {
 	cin >> c >> p >> k >> a >> b;
 	for (int i = 0; i < c; i++) {
-		dist[i] = 1e9;
 		int from,to,cost;
 		cin >> from >> to >> cost;
 		v[from].push_back({ to,cost });
 		v[to].push_back({ from,cost });
 	}
-	djk(a,b);
-	cout << dist[b];
+	djk(k);
+	int ks = dist[a] + dist[b];
+	djk(a);
+	int as = dist[k] + dist[b];
+	djk(b);
+	int bs = dist[k] + dist[a];
+
+	int mo = min(ks, as);
+	int mt = min(mo, bs);
+	cout << mt;
 
 	return 0;
 }
