101b47a458fd74b5ba4d02720d31d260f741ddce
agks <140994215+agks12@users.noreply.github.com>
2024-04-12 15:59:30 +0900

    Update 2023_up_pm1.cpp

--- codetree/2023_up_pm1.cpp
+++ codetree/2023_up_pm1.cpp
@@ -6,14 +6,14 @@ using namespace std;
 struct AtoE { int strow, endrow, stcol, endcol; };
 struct Pair { int row, col; };
 struct PairArr { bool TF = { 0 }; vector<int> v; };
-int N, M, K, nowLife, moveCnt, arr[11][11] ,copyArr[11][11];
+int N, M, K, nowLife, moveCnt, arr[11][11], copyArr[11][11];
 Pair parti[11];
 PairArr vArr[11][11], copyvArr[11][11]; //참가자와 출구만 있는 배열
 Pair Exit;
 int dr[4] = { -1,1,0,0 };
 int dc[4] = { 0,0,-1,1 };
-int dr8[8] = { -1,-1,0,-1,0,1,1,1 }; // 이거순서로 정사각형 만드는 법 달라짐
-int dc8[8] = { -1,0,-1,1,1,-1,0,1 };
+int dr8[8] = { -1,-1,0,1,1,-1,0,1 }; // 이거순서로 정사각형 만드는 법 달라짐
+int dc8[8] = { -1,0,-1,-1,0,1,1,1 };
 
 // 2점사이 거리계산
 int cal(int exitRow, int exitCol, int nowRow, int nowCol) {
@@ -28,7 +28,7 @@ void move() {
 		int nowRow = parti[i].row;
 		int nowCol = parti[i].col;
 
-		if (nowRow == -1)return;
+		if (nowRow == -1)continue;
 		int nowDist = cal(exitRow, exitCol, nowRow, nowCol);
 		int nowIndex = -1;
 		for (int j = 0; j < 4; j++) {
@@ -49,13 +49,33 @@ void move() {
 		parti[i].row += dr[nowIndex];
 		parti[i].col += dc[nowIndex];
 		// 참가자 배열 초기화(이때 기존값을 다음값으로
-		vArr[nowRow][nowCol].TF = false;
-		vArr[nowRow][nowCol].v.clear();
+		if (vArr[nowRow][nowCol].v.size() == 1) {
+			vArr[nowRow][nowCol].TF = false;
+			vArr[nowRow][nowCol].v.clear();
+		}
+		else {
+			//2명 이상이면 1새기 죽여야함 여기 파라미터는 변경 전 값
+			for (int k = 0; k < vArr[nowRow][nowCol].v.size(); k++) {
+				int nNode = vArr[nowRow][nowCol].v[k];//인덱스 1개만 추출
+
+				if (nNode == i) {
+					vArr[nowRow][nowCol].v.erase(vArr[nowRow][nowCol].v.begin() + k);
+					break;
+				}
+				//추출된 인덱스의 리스트값검사 이러면 중복 필요 없는데 인덱스만 제거 하는 거라서
+				//if ( (parti[nNode].row = nowRow) && (parti[nNode].col = nowCol) ) {
+				//	vArr[nowRow][nowCol].v.erase(vArr[nowRow][nowCol].v.begin() + k);
+				//	break;
+				//}
+			}
+		}
+
 
 		if (parti[i].row == Exit.row && parti[i].col == Exit.col) {
 			parti[i].row = -1;
 			parti[i].col = -1;
-			return;
+			nowLife--;
+			continue;
 		}
 		//참가자 배열 다음값 적용
 		vArr[parti[i].row][parti[i].col].TF = true;
@@ -63,28 +83,82 @@ void move() {
 	}
 }
 
-Pair findClose() {
+vector<Pair> findClose() {
 	queue<Pair> q;
+	//queue<Pair> q2;
+	vector<Pair> v;
+	int flag = 0;
+	int flag2 = 0;
 	q.push(Exit);
+	//q2.push(Exit);
 	int visited[11][11] = { 0 };
+	//int visited2[11][11] = { 0 };
 	visited[Exit.row][Exit.col] = 1;
+
+	//while (!q2.empty()) {
+	//	Pair now = q2.front(); q2.pop();
+	//	for (int i = 0; i < 8; i++) {
+	//		int nextRow = now.row + dr[i];
+	//		int nextCol = now.col + dc[i];
+	//		if (nextRow<1 || nextRow > N || nextCol < 1 || nextCol>N)continue;
+	//		if (visited2[nextRow][nextCol] > 0)continue;
+	//		visited2[nextRow][nextCol] = 1;
+	//		q2.push({ nextRow,nextCol });
+
+	//	}
+	//}
+
+
+	//while (!q.empty()) {
+	//	// 퍼저나가는 n+1 값이 정사각형 크기
+	//	Pair now = q.front(); q.pop();
+	//	if (flag2 == 1 && flag <= visited[now.row][now.col]) {
+	//		return v;
+	//	}
+	//	for (int i = 0; i < 8; i++) {
+	//		int nextRow = now.row + dr8[i];
+	//		int nextCol = now.col + dc8[i];
+	//		if (nextRow<1 || nextRow > N || nextCol < 1 || nextCol>N)continue;
+	//		if (visited[nextRow][nextCol] > 0)continue;
+	//		if (vArr[nextRow][nextCol].TF == true) {
+	//			//그냥 좌표만 반환하면 정사각형 어캐 판단하냐 // 가까운 모든 거 찾아서 가장 작은거 구해야됨
+	//			v.push_back({nextRow,nextCol});
+	//			flag = visited[now.row][now.col] +1;
+	//			flag2 = 1;
+	//			visited[nextRow][nextCol] = visited[now.row][now.col] + 1;
+	//			continue;
+	//		}
+	//		visited[nextRow][nextCol] = visited[now.row][now.col] + 1;
+	//		q.push({ nextRow,nextCol });
+	//	}
+	//}
+
 	while (!q.empty()) {
 		// 퍼저나가는 n+1 값이 정사각형 크기
 		Pair now = q.front(); q.pop();
+		if (flag2 == 1 && flag <= visited[now.row][now.col]) {
+			return v;
+		}
 		for (int i = 0; i < 8; i++) {
 			int nextRow = now.row + dr8[i];
 			int nextCol = now.col + dc8[i];
 			if (nextRow<1 || nextRow > N || nextCol < 1 || nextCol>N)continue;
 			if (visited[nextRow][nextCol] > 0)continue;
 			if (vArr[nextRow][nextCol].TF == true) {
-				//그냥 좌표만 반환하면 정사각형 어캐 판단하냐
-				return { nextRow,nextCol };
+				//그냥 좌표만 반환하면 정사각형 어캐 판단하냐 // 가까운 모든 거 찾아서 가장 작은거 구해야됨
+				v.push_back({nextRow,nextCol});
+				flag = visited[now.row][now.col] +1;
+				flag2 = 1;
+				visited[nextRow][nextCol] = visited[now.row][now.col] + 1;
+				continue;
 			}
-			visited[nextRow][nextCol] = visited[now.row][now.col]+1;
+			visited[nextRow][nextCol] = visited[now.row][now.col] + 1;
 			q.push({ nextRow,nextCol });
 		}
 	}
-	return { -1,-1 };
+	//v.clear();
+	//v.push_back({ -1,-1 });
+	return v;
 }
 
 int selectCross(Pair findPair) {
@@ -97,7 +171,93 @@ int selectCross(Pair findPair) {
 	else return -1; // 대각선위치
 }
 
-AtoE notCrossPair(Pair findPair) {
+// 이거 최대한 r 작고 c 작게 해야함
+//AtoE notCrossPair(Pair findPair) {
+//	int exitRow = Exit.row;
+//	int exitCol = Exit.col;
+//	int nowRow = findPair.row;
+//	int nowCol = findPair.col;
+//	// 가로세로에 위치경우
+//	//범위 초과하는지도 판단해야함
+//	//상하좌우 4가지 경우 각각에 대해 양옆으로 2가지 경우 있으므로 총 8가지 경우의수
+//	int stRow = 0;
+//	int endRow = 0;
+//	int stCol = 0;
+//	int endCol = 0;
+//
+//
+//	if (nowRow == exitRow && nowCol < exitCol) {
+//		//좌측
+//		int testLength = exitCol - nowCol;
+//		// 지금위치에서 특정 값 뺀값이 1이 아닌 최소값이 되야함
+//
+//		// nowRow = 5 , test = 7  => 5 -7  = -2  ( 이 값이 0보다 작으면 abs(차)만큼 
+//		if ((nowRow - testLength) >= 1) {//위에가 우선순위라 먼저검사
+//			stRow = nowRow - testLength;
+//			endRow = nowRow;
+//			stCol = nowCol;
+//			endCol = exitCol;
+//		}
+//		else {//아래쪽으로 
+//			stRow = nowRow;
+//			endRow = nowRow + testLength;
+//			stCol = nowCol;
+//			endCol = exitCol;
+//		}
+//	}
+//	else if (nowRow == exitRow && nowCol > exitCol) {
+//		//우측
+//		int testLength = nowCol - exitCol;
+//		if ((nowRow - testLength) >= 1) {//위에가 우선순위라 먼저검사
+//			stRow = nowRow - testLength;
+//			endRow = nowRow;
+//			stCol = exitCol;
+//			endCol = nowCol;
+//		}
+//		else {
+//			stRow = nowRow;
+//			endRow = nowRow + testLength;
+//			stCol = exitCol;
+//			endCol = nowCol;
+//		}
+//	}
+//	else if (nowCol == exitCol && nowRow > exitRow) {
+//		//하측
+//		int testLength = nowRow - exitRow;
+//		if ((nowCol - testLength) >= 1) {//위에가 우선순위라 먼저검사
+//			stRow = exitRow;
+//			endRow = nowRow;
+//			stCol = nowCol - testLength;
+//			endCol = exitCol;
+//		}
+//		else {
+//			stRow = exitRow;
+//			endRow = nowRow;
+//			stCol = exitCol;
+//			endCol = nowCol + testLength;
+//		}
+//	}
+//	else if (nowCol == exitCol && nowRow < exitRow) {
+//		//상측
+//		int testLength = exitRow - nowRow;
+//		if ((nowCol - testLength) >= 1) {//위에가 우선순위라 먼저검사
+//			stRow = nowRow;
+//			endRow = exitRow;
+//			stCol = nowCol - testLength;
+//			endCol = exitCol;
+//		}
+//		else {
+//			stRow = nowRow;
+//			endRow = exitRow;
+//			stCol = exitCol;
+//			endCol = nowCol + testLength;
+//		}
+//	}
+//
+//	return { stRow,endRow,stCol,endCol };
+//}
+
+AtoE notCrossPair2(Pair findPair) {
 	int exitRow = Exit.row;
 	int exitCol = Exit.col;
 	int nowRow = findPair.row;
@@ -114,18 +274,23 @@ AtoE notCrossPair(Pair findPair) {
 	if (nowRow == exitRow && nowCol < exitCol) {
 		//좌측
 		int testLength = exitCol - nowCol;
-		if ( (nowRow - testLength) >= 1) {//위에가 우선순위라 먼저검사
+		// 지금위치에서 특정 값 뺀값이 1이 아닌 최소값이 되야함
+
+		// nowRow = 5 , test = 7  => 5 -7  = -2  ( 이 값이 0보다 작으면 abs(차)만큼 
+		if ((nowRow - testLength) >= 1) {//위에가 우선순위라 먼저검사
 			stRow = nowRow - testLength;
 			endRow = nowRow;
 			stCol = nowCol;
 			endCol = exitCol;
 		}
-		else {
-			stRow = nowRow;
-			endRow = nowRow + testLength;
+		else if ((nowRow - testLength) < 1) {
+			//범위 초과 = > 무조건 row는 1부터 시작
+			stRow = 1; // 시작 
+			endRow = stRow +testLength; // 이건 현재에서 차이만큼 더함
 			stCol = nowCol;
 			endCol = exitCol;
 		}
+
 	}
 	else if (nowRow == exitRow && nowCol > exitCol) {
 		//우측
@@ -136,9 +301,10 @@ AtoE notCrossPair(Pair findPair) {
 			stCol = exitCol;
 			endCol = nowCol;
 		}
-		else {
-			stRow = nowRow;
-			endRow = nowRow + testLength;
+		else if ((nowRow - testLength) < 1) {
+			//범위 초과 = > 무조건 row는 1부터 시작
+			stRow = 1; // 시작 
+			endRow = stRow + testLength; // 이건 현재에서 차이만큼 더함
 			stCol = exitCol;
 			endCol = nowCol;
 		}
@@ -152,12 +318,14 @@ AtoE notCrossPair(Pair findPair) {
 			stCol = nowCol - testLength;
 			endCol = exitCol;
 		}
-		else {
-			stRow = exitRow;
-			endRow = nowRow;
-			stCol = exitCol;
-			endCol = nowCol + testLength;
+		else if ((nowCol - testLength) < 1) {
+			//범위 초과 = > 무조건 row는 1부터 시작
+			stRow = exitRow; // 시작 
+			endRow = nowRow ; // 이건 현재에서 차이만큼 더함
+			stCol = 1;
+			endCol = stCol + testLength;
 		}
+
 	}
 	else if (nowCol == exitCol && nowRow < exitRow) {
 		//상측
@@ -168,11 +336,12 @@ AtoE notCrossPair(Pair findPair) {
 			stCol = nowCol - testLength;
 			endCol = exitCol;
 		}
-		else {
-			stRow = nowRow;
-			endRow = exitRow;
-			stCol = exitCol;
-			endCol = nowCol + testLength;
+		else if ((nowCol - testLength) < 1) {
+			//범위 초과 = > 무조건 row는 1부터 시작
+			stRow = nowRow; // 시작 
+			endRow = exitRow; // 이건 현재에서 차이만큼 더함
+			stCol = 1;
+			endCol = stCol + testLength;
 		}
 	}
 
@@ -192,11 +361,12 @@ AtoE CrossPair(Pair findPair) {
 	int endCol = 0;
 
 	if (nowRow < exitRow && nowCol < exitCol) {
-		// 2사분면위치 
+		// 2사분면위치  row 는 그대로 사용 col 만 변경
 		stRow = nowRow;
 		endRow = exitRow;
 		stCol = nowCol;
 		endCol = exitCol;
+
 	}
 	else if (nowRow < exitRow && nowCol > exitCol) {
 		//1 사분면
@@ -220,7 +390,46 @@ AtoE CrossPair(Pair findPair) {
 		endCol = nowCol;
 	}
 
-	return { stRow,endRow,stCol,endCol };
+	if ((endRow - stRow) > (endCol - stCol)) {
+		//세로가 더 길어서 col늘려야 되는 경우
+		int testlength = endRow - stRow;//5
+		int nextStRow = stRow;//1
+		int nextEndRow = endRow;//6
+		int nextStCol = 0;
+		int nextEndCol = 0;
+		if (stCol - testlength >= 0) {
+			nextStCol = endCol - testlength;
+			nextEndCol = endCol;
+		}
+		else if (stCol - testlength < 0) {
+			nextStCol = 1;
+			nextEndCol = nextStCol + testlength;
+		}
+		return { nextStRow, nextEndRow, nextStCol, nextEndCol };
+	}
+	else if ((endRow - stRow) < (endCol - stCol)) {
+		//가로가 더 길어서 row 늘려야 되는 경우
+		int testlength = endCol - stCol;
+		int nextStRow = 0;
+		int nextEndRow = 0;
+		int nextStCol = stCol;
+		int nextEndCol = endCol;
+		if (stRow - testlength >= 0) {
+			nextStRow = endRow - testlength;
+			nextEndRow = endRow;
+		}
+		else if (stRow - testlength < 0) {
+			nextStRow = 1;
+			nextEndRow = nextStRow + testlength;
+		}
+
+		return { nextStRow, nextEndRow, nextStCol, nextEndCol };
+	}
+	else if ((endRow - stRow) == (endCol - stCol)) {
+		// 그냥 지금 대로 반환
+		return { stRow,endRow,stCol,endCol };
+	}
+
 }
 
 AtoE subTurn(Pair findPair) {
@@ -230,8 +439,8 @@ AtoE subTurn(Pair findPair) {
 	//이새끼들은 좌상, 우하 반환하는 역활
 	int crossT = selectCross(findPair);
 	AtoE doubPair = { 0 };
-	if (crossT == 1)doubPair = notCrossPair(findPair);
-	else doubPair = CrossPair(findPair);
+	if (crossT == 1)doubPair = notCrossPair2(findPair); 
+	else doubPair = CrossPair(findPair); // 대각선이면 그대로 해도됨
 
 	//무조건 좌상우하 반환
 	return doubPair;
@@ -245,6 +454,10 @@ void turn(AtoE area) {
 	int stCol = area.stcol;
 	int endCol = area.endcol;
 
+	//cout << "\n" << stRow << "," << stCol << "  " << endRow << "," << endCol << "\n";
+
+	//if ((stRow - endRow) != (stCol - endCol))cout << "\n nonononononononononono\n";
+
 	for (int i = stRow; i <= endRow; i++) {
 		for (int j = stCol; j <= endCol; j++) {
 			copyArr[i][j] = arr[i][j];
@@ -257,7 +470,7 @@ void turn(AtoE area) {
 			arr[stRow + j - stCol][endCol - i + stRow] = copyArr[i][j] - 1;
 			if (arr[stRow + j - stCol][endCol - i + stRow] < 0)arr[stRow + j - stCol][endCol - i + stRow] = 0;
 			vArr[stRow + j - stCol][endCol - i + stRow] = copyvArr[i][j];
-			if (vArr[stRow + j - stCol][endCol - i + stRow].TF == true && vArr[stRow + j - stCol][endCol - i + stRow].v.size()!=0) {
+			if (vArr[stRow + j - stCol][endCol - i + stRow].TF == true && vArr[stRow + j - stCol][endCol - i + stRow].v.size() != 0) {
 				for (int k = 0; k < vArr[stRow + j - stCol][endCol - i + stRow].v.size(); k++) {
 					int nowNode = vArr[stRow + j - stCol][endCol - i + stRow].v[k];
 					parti[nowNode].row = stRow + j - stCol;
@@ -272,7 +485,7 @@ void turn(AtoE area) {
 	}
 
 	//리스트 및 exit 값 초기화
-	
+
 }
 
 int main() {
@@ -283,7 +496,7 @@ int main() {
 		}
 	}
 
-	
+
 	//참가자 좌표
 	for (int i = 0; i < M; i++) {
 		int r, c;
@@ -302,44 +515,89 @@ int main() {
 	nowLife = M;//생존자
 
 	//이동함수
-	while (K>0) {
+	while (K > 0) {
 
-		move();
-		Pair findPair = findClose();
-		if (findPair.row == -1)break;//사람새끼 없음
-		AtoE insertTurn = subTurn(findPair); //양옆이 아니라 가운데 처리 안함
-		turn(insertTurn);
 
-		cout << "===========================\n";
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				cout<< arr[i][j]<<" ";
-			}
-			cout << "\n";
-		}
-		cout << "===========================\n";
-		cout << "EXIT" << Exit.row << " " << Exit.col<<"Exit\n";
-		for (int i = 0; i < M; i++) {
-			cout << " "<<parti[i].row << "," << parti[i].col<<" ";
-		}
-		cout << "\n";
+		//cout << "====  " << K << "  ====== = \n";
 		//for (int i = 1; i <= N; i++) {
 		//	for (int j = 1; j <= N; j++) {
-		//		PairArr now= vArr[i][j];
-		//		cout <<"bool" << now.TF<<"bool ";
-		//		for (int k = 0; k < now.v.size(); k) {
-		//			cout << now.v[k] << " ";
-		//		}
-		//		cout << "    ";
+		//		cout << arr[i][j] << " ";
+		//	}
+		//	cout << "   ";
+		//	for (int j = 1; j <= N; j++) {
+		//		cout << vArr[i][j].TF << " ";
 		//	}
 		//	cout << "\n";
 		//}
+		//cout << "========" << K << "  =============\n";
+		//cout << "EXIT" << Exit.row << " " << Exit.col << "Exit\n";
+		move();
+
+		//cout << "====  " << K << "  ====== = \n";
+		//for (int i = 1; i <= N; i++) {
+		//	for (int j = 1; j <= N; j++) {
+		//		cout << arr[i][j] << " ";
+		//	}
+		//	cout << "   ";
+		//	for (int j = 1; j <= N; j++) {
+		//		cout << vArr[i][j].TF << " ";
+		//	}
+		//	cout << "\n";
+		//}
+		//cout << "========" << K << "  =============\n";
+		vector<Pair> findPair = findClose();
 
 		if (nowLife == 0)break;
+		if (findPair.size() == 0)break;
+		if (findPair[0].row == -1 && findPair[0].col==-1)break;//사람새끼 없음
+
+		int row = 100;
+		int col = 100;
+		AtoE minTurn = { 0 };
+		for (int ch = 0; ch < findPair.size(); ch++) {
+			AtoE insertTurn = subTurn(findPair[ch]); //양옆이 아니라 가운데 처리 안함
+			if (insertTurn.strow < row) {
+				row = insertTurn.strow;
+				col = insertTurn.stcol;
+				minTurn = insertTurn;
+			}
+			else if (insertTurn.strow == row) {
+				if (insertTurn.stcol < col) {
+					row = insertTurn.strow;
+					col = insertTurn.stcol;
+					minTurn = insertTurn;
+				}
+			}
+		}
+		
+		turn(minTurn);
+
+		//cout << "====  "<<K<<"  ====== = \n";
+		//for (int i = 1; i <= N; i++) {
+		//	for (int j = 1; j <= N; j++) {
+		//		cout << arr[i][j] << " ";
+		//	}
+		//	cout <<  "   ";
+		//	for (int j = 1; j <= N; j++) {
+		//		cout << vArr[i][j].TF << " ";
+		//	}
+		//	cout << "\n";
+		//}
+		//cout << "========" << K << "  =============\n";
+		////cout << "find" << findPair.row << " " << findPair.col << "find\n";
+		//for (int i = 0; i < M; i++) {
+		//	cout << " " << parti[i].row << "," << parti[i].col << " ";
+		//}
+		//cout << "\n\n\n\n";
+		// 
+		 
+
+
+		
 		K--;
 	}
-	
-	cout <<moveCnt<<"\n" <<Exit.row << " " << Exit.col;
+
+	cout << moveCnt << "\n" << Exit.row << " " << Exit.col;
 	int s = 0;
 
 	return 0;
