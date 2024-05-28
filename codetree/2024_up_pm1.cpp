dc9f8914ba852a3c83a9f81bbce9522c8d4179c7
agks <140994215+agks12@users.noreply.github.com>
2024-05-28 23:31:49 +0900

    Update 2024_up_pm1.cpp

--- codetree/2024_up_pm1.cpp
+++ codetree/2024_up_pm1.cpp
@@ -1,13 +1,19 @@
 #include<iostream>
+#include<queue>
 using namespace std;
 
-struct map {int index, exitFlag;};
+// dp안쓰고 dfs쓰면 오져서 시간초과 bfs쓰면 큐에 존나 많이 담겨서 메모리 초과 되는듯
+
+
+//struct map {int index, exitFlag;};
 struct Pair { int row, col; };
+struct Pindex { int row, col, index; };
+
 int R, C, K;
-map arr[71][71];
+int arr[71][71];
 Pair indexMatchExit[1001];
 int indexMatchCenter[1001];
-int visited[1001];
+//int visited[1001];
 int maxValue = -1;
 int sumScore;
 
@@ -33,8 +39,8 @@ int Ldc[5] = {-1,-2,-2,-1,-1}; // 오른쪽은 부호 반대
 void Reset(int index) {
 	for (int i = 0; i < R; i++) {
 		for (int j = 0; j < C; j++) {
-			arr[i][j].exitFlag = 0;
-			arr[i][j].index = 0;
+			//arr[i][j].exitFlag = 0;
+			arr[i][j] = 0;
 		}
 	}
 	// 이거 할 필요 없음 어차피 인덱스 한번 쓰면 다시 안씀 배열만 초기화하면 쓸일없음
@@ -48,7 +54,7 @@ bool goDown(Pair nowPoi, int index) {
 	for (int i = 0; i < 3; i++) {
 		int nextRow = nowPoi.row + Ddr[i];
 		int nextCol = nowPoi.col + Ddc[i];
-		if (nextCol < 0 || nextCol >= C || nextRow >= R || (nextRow >= 0 &&  arr[nextRow][nextCol].index>0 && arr[nextRow][nextCol].index != index)) {
+		if (nextCol < 0 || nextCol >= C || nextRow >= R || (nextRow >= 0 &&  arr[nextRow][nextCol]>0 && arr[nextRow][nextCol] != index)) {
 			// 지도 초과 및 우주선 있는 경우 못감
 			return false;
 		}
@@ -60,7 +66,7 @@ bool goLR(Pair nowPoi, int dir, int index) {
 	for (int i = 0; i < 5; i++) {
 		int nextRow = nowPoi.row + LRdr[i];
 		int nextCol = nowPoi.col + (dir *Ldc[i]); {
-			if (nextCol < 0 || nextCol >= C || nextRow >= R || (nextRow >=0 && arr[nextRow][nextCol].index > 0 && arr[nextRow][nextCol].index != index)) {
+			if (nextCol < 0 || nextCol >= C || nextRow >= R || (nextRow >=0 && arr[nextRow][nextCol] > 0 && arr[nextRow][nextCol] != index)) {
 				// 지도 초과 및 우주선 있는 경우 못감
 				return false;
 			}
@@ -73,11 +79,11 @@ bool goLR(Pair nowPoi, int dir, int index) {
 void moveDown(Pair nowPoi, int index) {
 	if (nowPoi.row >= 1){
 		arr[nowPoi.row + 1][nowPoi.col - 1] = arr[nowPoi.row][nowPoi.col - 1];
-		arr[nowPoi.row][nowPoi.col - 1] = { 0,0 };
+		arr[nowPoi.row][nowPoi.col - 1] = { 0};
 		arr[nowPoi.row + 1][nowPoi.col + 1] = arr[nowPoi.row][nowPoi.col + 1];
-		arr[nowPoi.row][nowPoi.col + 1] = { 0,0 };
+		arr[nowPoi.row][nowPoi.col + 1] = { 0};
 		arr[nowPoi.row + 2][nowPoi.col] = arr[nowPoi.row - 1][nowPoi.col];
-		arr[nowPoi.row - 1][nowPoi.col] = { 0,0 };
+		arr[nowPoi.row - 1][nowPoi.col] = { 0};
 	}
 	else {
 		for (int i = 0; i < 5; i++) {
@@ -85,7 +91,7 @@ void moveDown(Pair nowPoi, int index) {
 			int nextRow = nowPoi.row + dr[i];
 			int nextCol = nowPoi.col + dc[i];
 			if (nextRow < 0 || nextCol < 0 || nextRow >= R || nextCol >= C)continue;
-			arr[nextRow][nextCol].index = 0;
+			arr[nextRow][nextCol] = 0;
 		}
 		nowPoi.row += 1;
 		for (int i = 0; i < 5; i++) {
@@ -93,7 +99,7 @@ void moveDown(Pair nowPoi, int index) {
 			int nextRow = nowPoi.row + dr[i];
 			int nextCol = nowPoi.col + dc[i];
 			if (nextRow < 0 || nextCol < 0 || nextRow >= R || nextCol >= C)continue;
-			arr[nextRow][nextCol].index = index;
+			arr[nextRow][nextCol] = index;
 		}
 	}
 }
@@ -101,11 +107,11 @@ void moveDown(Pair nowPoi, int index) {
 void moveLR(Pair nowPoi, int LR, int index) {
 	if (nowPoi.row >= 1) {
 		arr[nowPoi.row + 1][nowPoi.col + 1 * LR] = arr[nowPoi.row][nowPoi.col];
-		arr[nowPoi.row][nowPoi.col] = { 0,0 };
+		arr[nowPoi.row][nowPoi.col] = { 0};
 		arr[nowPoi.row + 1][nowPoi.col + 2 * LR] = arr[nowPoi.row - 1][nowPoi.col];
-		arr[nowPoi.row - 1][nowPoi.col] = { 0,0 };
+		arr[nowPoi.row - 1][nowPoi.col] = { 0};
 		arr[nowPoi.row + 2][nowPoi.col + 1 * LR] = arr[nowPoi.row][nowPoi.col - 1 * LR];
-		arr[nowPoi.row][nowPoi.col - 1 * LR] = { 0,0 };
+		arr[nowPoi.row][nowPoi.col - 1 * LR] = { 0};
 	}
 	else {
 		for (int i = 0; i < 5; i++) {
@@ -113,7 +119,7 @@ void moveLR(Pair nowPoi, int LR, int index) {
 			int nextRow = nowPoi.row + dr[i];
 			int nextCol = nowPoi.col + dc[i];
 			if (nextRow < 0 || nextCol < 0 || nextRow >= R || nextCol >= C)continue;
-			arr[nextRow][nextCol].index = 0;
+			arr[nextRow][nextCol] = 0;
 		}
 		// 이 점을 중심으로 십자가 채우면 알아서 회전경우 됨
 		nowPoi.row += 1;
@@ -123,38 +129,76 @@ void moveLR(Pair nowPoi, int LR, int index) {
 			int nextRow = nowPoi.row + dr[i];
 			int nextCol = nowPoi.col + dc[i];
 			if (nextRow < 0 || nextCol < 0 || nextRow >= R || nextCol >= C)continue;
-			arr[nextRow][nextCol].index = index;
+			arr[nextRow][nextCol] = index;
 		}
 	}
 }
 
-int flags;
-void getDfs(Pair nowPoi, int index) {
-	if (flags == 1)return;
-	int nowMaxRow = indexMatchCenter[index]+2;
-	if (nowMaxRow > maxValue)maxValue = nowMaxRow;
-	if (maxDp[index] == R) {
-		maxValue = R;
-		flags = 1;
-		return;
-	}
-	if (maxValue == R) {
-		flags = 1;
-		return;
-	}
-	//cout << "\n 지금 위치 좌표" << nowPoi.row << "," << nowPoi.col << "지금 최대값" << maxValue << "\n";
-	for (int i = 0; i < 4; i++) {
-		int nextRow = nowPoi.row + Edr[i];
-		int nextCol = nowPoi.col + Edc[i];
-		if (nextRow < 0 || nextCol < 0 || nextRow >= R || nextCol >= C)continue;
-		// 없는칸이거나 내우주선이면 넘김
-		if (arr[nextRow][nextCol].index == 0 || arr[nextRow][nextCol].index == index)continue;
-		if (visited[index] == 1)continue;
-		visited[index] = 1;
-		int nextIndex = arr[nextRow][nextCol].index;
-		Pair nextPoi = indexMatchExit[nextIndex];
-		getDfs(nextPoi, nextIndex);
-		visited[index] = 0; // 초기화될려나
+//int flags;
+//void getDfs(Pair nowPoi, int index) {
+//	if (flags == 1)return;
+//	int nowMaxRow = indexMatchCenter[index]+2;
+//	if (nowMaxRow > maxValue)maxValue = nowMaxRow;
+//	if (maxDp[index] == R) {
+//		maxValue = R;
+//		flags = 1;
+//		return;
+//	}
+//	if (maxValue == R) {
+//		flags = 1;
+//		return;
+//	}
+//	//cout << "\n 지금 위치 좌표" << nowPoi.row << "," << nowPoi.col << "지금 최대값" << maxValue << "\n";
+//	for (int i = 0; i < 4; i++) {
+//		int nextRow = nowPoi.row + Edr[i];
+//		int nextCol = nowPoi.col + Edc[i];
+//		if (nextRow < 0 || nextCol < 0 || nextRow >= R || nextCol >= C)continue;
+//		// 없는칸이거나 내우주선이면 넘김
+//		if (arr[nextRow][nextCol].index == 0 || arr[nextRow][nextCol].index == index)continue;
+//		if (visited[index] == 1)continue;
+//		visited[index] = 1;
+//		int nextIndex = arr[nextRow][nextCol].index;
+//		Pair nextPoi = indexMatchExit[nextIndex];
+//		getDfs(nextPoi, nextIndex);
+//		visited[index] = 0; // 초기화될려나
+//	}
+//}
+
+
+int visited[1001];
+void getBfs(Pair nowPoi, int index) {
+	queue<Pindex> q;
+	q.push({nowPoi.row,nowPoi.col ,index});
+	visited[index] = 1;
+	int flag = 0;
+	while (!q.empty()) {
+		Pindex now = q.front(); q.pop();
+		//Pair nowExit = indexMatchExit[index];
+
+		int nowIndex = now.index;
+		int nowMaxRow = indexMatchCenter[nowIndex] + 2;
+		if (nowMaxRow > maxValue)maxValue = nowMaxRow;
+
+		if (maxValue == R) {
+			break;
+		}
+		for (int i = 0; i < 4; i++) {
+			int nextRow = now.row + Edr[i];
+			int nextCol = now.col + Edc[i];
+			if (arr[nextRow][nextCol] == 0 || arr[nextRow][nextCol] == nowIndex)continue;
+			int nextIndex = arr[nextRow][nextCol];
+			if (visited[nextIndex] == 1)continue;
+			Pair nextExit = indexMatchExit[nextIndex];
+			if (maxDp[nowIndex] != 0) {
+				if (maxDp[nowIndex] > maxValue) {
+					flag = 1;
+					maxValue = maxDp[nowIndex];
+				}
+				//cout << "\n dp사용" << nowIndex;
+			}
+			q.push({ nextExit.row, nextExit.col,nextIndex });
+		}
+		if (flag == 1)break;
 	}
 }
 
@@ -199,13 +243,14 @@ void startCol(int index) {
 			// 배열에 지금 착률한애 출구 표시
 			indexMatchExit[index] = {(nowPoi.row + Edr[exitDir]), (nowPoi.col + Edc[exitDir])};
 			indexMatchCenter[index] = nowPoi.row; // 무조건 이거보다 1큼
-			arr[nowPoi.row + Edr[exitDir]][nowPoi.col + Edc[exitDir]].exitFlag = 1;
+			//arr[nowPoi.row + Edr[exitDir]][nowPoi.col + Edc[exitDir]].exitFlag = 1;
 			// 이후 점수계산
 			Pair nowExit = indexMatchExit[index];
 			maxValue = -1;
-			flags = 0;
-			getDfs(nowExit,index);
+			//flags = 0;
+			getBfs(nowExit,index);
 			maxDp[index] = maxValue;
+			for (int i = 0; i <= index; i++)visited[i] = 0;
 			//cout << "\n"<<"maxVlaue"<< maxValue <<"\n";
 			sumScore += maxValue;
 		}
@@ -219,7 +264,7 @@ int main() {
 		startCol(i); // 1 우주선 출발
 		//for (int j = 0; j < R; j++) {
 		//	for (int k = 0; k < C; k++) {
-		//		cout << arr[j][k].index << " ";
+		//		cout << arr[j][k] << " ";
 		//	}
 		//	cout << "\n";
 		//}
